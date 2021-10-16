c161f6aafcb30109ce3124c0c4a3d0e4fa269580
NathOrmond <nath.m.ormond@gmail.com>
2021-10-16 14:38:22 +0100

    add readme stuff

---
+++ README.md
@@ -0,0 +1,8 @@
+# About 
+
+# How to Run 
+
+# Contributing
+
+# Deployment 
+This application is deployed at: https://nt-redaction-api.herokuapp.com/
--- app/testread.py
+++ app/testread.py
@@ -1,5 +1,10 @@
 import os, sys
 
-# Open a file
+def get_file_contents(filename): 
+  fd = os.open(filename, os.O_RDWR)
+  ret = os.read(fd, os.path.getsize(filename))
+  rval = ret.decode("utf-8")
+  os.close(fd)
+  return ret
 
-
+get_file_contents
