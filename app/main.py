f79f1ad1c3346d24875235bcf08f55679778731e
NathOrmond <nath.m.ormond@gmail.com>
2021-10-16 14:35:54 +0100

    save but still WIP

--- app/main.py
+++ app/main.py
@@ -22,13 +22,8 @@ def display():
     mss_list = os.listdir('./testmss')
     content = ''
     if mss_name in mss_list: 
-      # TODO Read document and populate response with its contents
       filename = './testmss/' + mss_name
-      fd = os.open(filename, os.O_RDWR)
-      ret = os.read(fd, 12)
-      print(ret)
-      content = filename
-      os.close(fd)
+      content = get_file_contents(filename)
     else: 
       content ='Manuscript not found'
     response = {
@@ -56,3 +51,10 @@ def levenshtein():
         }
     }
     return jsonify(response)
+
+def get_file_contents(filename): 
+  fd = os.open(filename, os.O_RDWR)
+  ret = os.read(fd, os.path.getsize(filename))
+  rval = ret.decode("utf-8")
+  os.close(fd)
+  return rval
---
+++ app/testread.py
@@ -0,0 +1,5 @@
+import os, sys
+
+# Open a file
+
+
